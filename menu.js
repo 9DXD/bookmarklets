3eb30621f05a2cb1d849af2c9bcae5dc0c479fc4
9DXD <93220917+9DXD@users.noreply.github.com>
2023-12-12 07:48:19 -0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 javascript:(function(){
  var popup = window.open("", "_blank", "width=300,height=200");
- popup.document.write("<html><head><title>Blank Page</title></head><body><h1>This is a blank page</h1></body></html>");
+ popup.document.write("<html><head><title>about:blank</title></head><body><h1>Bookmarklet Menu</h1></body></html>");
  /*popup.document.close();
  window.onblur = function() {
   popup.close();
