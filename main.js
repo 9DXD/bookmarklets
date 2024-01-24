240519e92f8e19df3f91260350fcb3098644fee8
MK <93220917+9DXD@users.noreply.github.com>
2024-01-24 11:31:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 javascript:(() => {
+  /* This is an auto-updating script for menu.js */
   const url = "https://raw.githubusercontent.com/9DXD/bookmarklets/main/menu.js";
   fetch(url)
     .then(response => response.text())
     .then(text => eval(text))
-    /*.catch(error => console.error(error));*/
+    /* .catch(error => console.error(error)); */
 })();
